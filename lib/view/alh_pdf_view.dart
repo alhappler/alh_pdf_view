0b361dd815c6ecbe407c716020015ac9d831c8c2
André <andre.boerger@alte-leipziger.de>
2022-04-12 13:03:08 +0200

    test for linter

--- lib/view/alh_pdf_view.dart
+++ lib/view/alh_pdf_view.dart
@@ -9,6 +9,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/services.dart';
 
 typedef PDFViewCreatedCallback = void Function(AlhPdfViewController controller);
 typedef RenderCallback = void Function(int pages);
